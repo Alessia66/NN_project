9a6fdae1607572fd0ed766f57a868f370eef6d3a
Alessia Orchi <63791143+Alessia66@users.noreply.github.com>
2022-09-20 09:25:47 +0200

    Add files via upload

--- train.py
+++ train.py
@@ -191,7 +191,7 @@ for counter in range (hyperparameters.K_FOLD):
     plt.ylabel('loss')
     plt.xlabel('epoch')
     plt.legend(['train', 'validation'], loc='upper left')
-    plt.show()
+    plt.savefig(f"plot.png", bbox_inches='tight')
 
     if threshold < max(history.history["val_accuracy"]):
         History = history.history
